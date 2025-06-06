0d485ef09eaeedd720731b41c14e37de44113144
EliotGrd <eliotgrd@protonmail.com>
2025-06-06 17:12:14 +0200

    Update colorscheme.lua

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -2,6 +2,10 @@ return {
 	"rose-pine/neovim",
 	name = "rose-pine",
 	config = function()
+		local rose_pine = require("rose-pine")
+		rose_pine.setup({
+			styles = { italic = false }
+		})
 		vim.cmd("colorscheme rose-pine")
 	end
 }
